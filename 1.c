a0f157e22d2a0844412a1d4ffd855f0bdb063992
adharshchandranpv7-del <adharshchandranpv7@gmail.com>
2026-02-05 22:45:53 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+
+my_list = [10, 20, 30, 40, 50]
+
+
+num = int(input("Enter the number to insert: "))
+pos = int(input("Enter the position: "))
+
+
+my_list.insert(pos, num)
+
+
+print("Updated list:", my_list)
